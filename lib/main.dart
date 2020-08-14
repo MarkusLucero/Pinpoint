5d88b811a0ce65bf7843108dd34bf0c43c570376
MarkusLucero <markuslucero@hotmail.com>
2020-08-14 17:00:49 +0200

    added a src folder, updated the main.dart to only render the map widget and created an empty map widget in pinpoint_map.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'src/pinpoint_map.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,15 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      title: 'Welcome to our app!',
+      home: PinPointMap(),
     );
   }
 }
---
+++ lib/src/pinpoint_map.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class PinPointMap extends StatefulWidget {
+  //Override because the widget manages its own state
+  @override
+  //Builds the PinPointMapState widget.
+  _PinPointMapState createState() => _PinPointMapState();
+}
+
+class _PinPointMapState extends State<PinPointMap> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
