6f6a410b6d07c37375338ec20f1286680fad86a1
MarkusLucero <markuslucero@hotmail.com>
2020-08-14 21:12:06 +0200

    Added google maps with a simple controller to allow zooming in/panning around

--- lib/src/pinpoint_map.dart
+++ lib/src/pinpoint_map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class PinPointMap extends StatefulWidget {
@@ -11,8 +12,18 @@ class PinPointMap extends StatefulWidget {
 /// This widget will create the map layout and define the map attributes
 
 class _PinPointMapState extends State<PinPointMap> {
+  Completer<GoogleMapController> _controller = Completer();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: GoogleMap(
+      mapType: MapType.normal,
+      initialCameraPosition:
+          CameraPosition(target: LatLng(59.837604, 17.634548), zoom: 18),
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+      },
+    ));
   }
 }
