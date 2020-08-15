507995da4d98511d4294162d45dac9a3590d6bb1
Travis Persson <travispersson93@gmail.com>
2020-08-15 19:09:14 +0200

    need to decide how the cards should look like

--- lib/src/pinpoint_card.dart
+++ lib/src/pinpoint_card.dart
@@ -2,17 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:pinpoint/src/models/pin_point_model.dart';
 
 class PinPointCard extends StatelessWidget {
-
   // Test PinPoint data
   final PinPoint pinPoint = PinPoint(
     title: "The best location",
     notes: "this place was actually pretty cool let me tell u that my friends",
-    imgUrl: null,
+    imgUrl: "https://medioteket.gavle.se/assets/img/error/img.png",
     location: "Uppsala",
   );
 
   @override
   Widget build(BuildContext context) {
-    return Card();
+    return Card(
+      child: Container(), 
+    );
   }
 }
