a804323f4fcb15d3ab077ca562540bac9c77b2e7
MarkusLucero <markuslucero@hotmail.com>
2020-08-16 20:46:15 +0200

    Added functionality to add markers on the map. The markers can then be clicked and display their address

--- lib/src/pinpoint_map.dart
+++ lib/src/pinpoint_map.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geocoder/geocoder.dart';
 
 class PinPointMap extends StatefulWidget {
   //Override because the widget manages its own state
@@ -12,17 +13,50 @@ class PinPointMap extends StatefulWidget {
 /// This widget creates the map layout and define the map attributes
 class _PinPointMapState extends State<PinPointMap> {
   Completer<GoogleMapController> _controller = Completer();
+  Set<Marker> _markers = Set<Marker>();
+  String _address = "";
+
+  void _handleTap(LatLng point) async {
+    await _findPlaceFromCoords(point);
+    setState(() {
+      _markers.add(Marker(
+          markerId: MarkerId(point.toString()),
+          position: point,
+          infoWindow: InfoWindow(title: _address, snippet: "Test"),
+          icon: BitmapDescriptor.defaultMarker));
+    });
+  }
+
+  Future<void> _findPlaceFromCoords(LatLng point) async {
+    final coordinates = new Coordinates(point.latitude, point.longitude);
+    print(coordinates);
+    List<Address> address =
+        await Geocoder.local.findAddressesFromCoordinates(coordinates);
+    setState(() {
+      _address = address.first.thoroughfare +
+          " " +
+          address.first.subThoroughfare +
+          ", " +
+          address.first.adminArea;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: GoogleMap(
-      mapType: MapType.normal,
-      initialCameraPosition:
-          CameraPosition(target: LatLng(59.837604, 17.634548), zoom: 18),
-      onMapCreated: (GoogleMapController controller) {
-        _controller.complete(controller);
-      },
-    ));
+
+        ///Stack allows multiple widgets to be placed ontop of eachother. E.g stuff on the map.
+        body: Stack(children: <Widget>[
+      GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition:
+            CameraPosition(target: LatLng(59.837604, 17.634548), zoom: 18),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        onTap: _handleTap,
+        markers: _markers,
+      )
+    ]));
   }
 }
