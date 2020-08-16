cb7a2ca38e22e837244f2a31ded44941b570691c
MarkusLucero <markuslucero@hotmail.com>
2020-08-16 20:48:04 +0200

    added some comments

--- lib/src/pinpoint_map.dart
+++ lib/src/pinpoint_map.dart
@@ -13,9 +13,13 @@ class PinPointMap extends StatefulWidget {
 /// This widget creates the map layout and define the map attributes
 class _PinPointMapState extends State<PinPointMap> {
   Completer<GoogleMapController> _controller = Completer();
+
+  ///This set contains all markers placed on the map
   Set<Marker> _markers = Set<Marker>();
+
   String _address = "";
 
+  ///Function that handles the tap event. I.e clicking to set a marker
   void _handleTap(LatLng point) async {
     await _findPlaceFromCoords(point);
     setState(() {
@@ -27,6 +31,7 @@ class _PinPointMapState extends State<PinPointMap> {
     });
   }
 
+  ///Function that gets the coordinates of the marker and sets the address accordingly"
   Future<void> _findPlaceFromCoords(LatLng point) async {
     final coordinates = new Coordinates(point.latitude, point.longitude);
     print(coordinates);
